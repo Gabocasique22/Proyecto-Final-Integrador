10c75fd40d61f9a7444bdd11c69b3fae466d15bc
Gabocasique22 <qa74.gabo@gmail.com>
2023-02-01 00:09:13 -0300

    001.Primer commit

---
+++ JavaScript/Funciones-ProyectoFinal.js
@@ -0,0 +1,32 @@
+
+//Función 1.
+document.getElementById('boton1').onclick = function () {
+
+    document.getElementById('demoINTERESES').innerHTML = "Deportes - Economía - Historia - Tecnología - Jazz - Web3 - Blockchaing - Bases de datos - Automotion Testing"
+}
+
+
+//Función 2.
+document.getElementById('boton2').onclick = function () {
+
+    document.getElementById('demoINTERESES').innerHTML = "Info OCULTA"
+}
+
+////Función 3.
+document.getElementById('boton3').onclick = function () {
+
+    document.getElementById('demoINTERESES').innerHTML = "Presiona el boton para saber mas sobre mis intereses en general."
+}
+
+
+
+
+////Función 4.
+document.getElementById('boton-color4').addEventListener('click', function () {
+document.body.style.backgroundColor = 'Black';
+});
+
+////Función 5.
+document.getElementById('boton-color5').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'rgb(2, 105, 201)';
+    });
